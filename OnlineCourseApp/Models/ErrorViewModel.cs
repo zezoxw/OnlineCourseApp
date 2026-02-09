3d9a7fb3839839478f28d0c40113455a1279d0a5
rozana <zezoxw23@gmail.com>
2026-02-09 18:10:08 +0300

    Add project files.

---
+++ OnlineCourseApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineCourseApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
